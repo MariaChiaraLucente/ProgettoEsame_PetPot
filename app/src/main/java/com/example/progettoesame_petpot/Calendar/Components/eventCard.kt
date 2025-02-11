dad8bb46c3e17434146cac9a57943183e0b2c7db
Maria Chiara Lucente <s319443@studenti.polito.it>
2025-02-11 11:47:53 +0100

    sono mari cambio giusto una cosa

--- app/src/main/java/com/example/progettoesame_petpot/Calendar/Components/eventCard.kt
+++ app/src/main/java/com/example/progettoesame_petpot/Calendar/Components/eventCard.kt
@@ -16,9 +16,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
@@ -50,9 +52,10 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import com.example.progettoesame_petpot.ui.CalendarGridFeed
+
 import java.text.SimpleDateFormat
 import java.util.Calendar
+import java.util.Date
 import java.util.Locale
 
 
@@ -536,5 +539,114 @@ fun FeedCreationScreen(
                 }
             }
         )
+    }
+}
+
+@Composable
+fun CalendarGridFeed(
+    calendarViewModel: EventViewModel,
+    daysInMonth: Int,
+    currentMonth: Int,
+    currentYear: Int,
+    selectedStartDate: Date?,
+    selectedEndDate: Date?,
+    onDayClick: (Int) -> Unit
+) {
+    val calendar = Calendar.getInstance()
+    val dayNames = listOf("Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat")
+
+    Card(
+        shape = RoundedCornerShape(16.dp),
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(330.dp)
+            .padding(16.dp),
+        colors = CardDefaults.cardColors(containerColor = Color(0xFF5576B4))
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.fillMaxSize()
+        ) {
+
+            // Row for day names
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                dayNames.forEach { dayName ->
+                    Text(
+                        text = dayName,
+                        color = Color.White,
+                        fontSize = 16.sp,
+                        modifier = Modifier.padding(4.dp)
+                    )
+                }
+            }
+            Spacer(modifier = Modifier.height(8.dp))
+
+            // Rows for days in the month
+            for (week in 0 until 6) {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceEvenly
+                ) {
+                    for (day in 1..7) {
+                        val currentDay = week * 7 + day
+                        if (currentDay <= daysInMonth) {
+                            // Create the date corresponding to the current day
+                            calendar.set(Calendar.YEAR, currentYear)
+                            calendar.set(Calendar.MONTH, currentMonth)
+                            calendar.set(Calendar.DAY_OF_MONTH, currentDay)
+                            val currentDate = calendar.time
+
+                            // Check if the day is selected or in the range
+                            val isSelected = calendarViewModel.isDateSelected(
+                                currentDate,
+                                selectedStartDate,
+                                selectedEndDate
+                            )
+                            val hasFeed = calendarViewModel.isFeedDay(currentDate)
+                            val backgroundColor = when {
+                                isSelected -> Color(0xFF1F2B85) // Selected color
+
+                                else -> Color.Blue.copy(alpha = 0.3f)
+                            }
+                            Column {
+                                Box(
+                                    modifier = Modifier
+                                        .size(35.dp)
+                                        .background(backgroundColor, CircleShape)
+                                        .clickable { onDayClick(currentDay) },
+                                    contentAlignment = Alignment.Center
+                                ) {
+                                    Text(
+                                        text = currentDay.toString(),
+                                        color = Color.White,
+                                        fontSize = 16.sp
+                                    )
+                                }
+                                if (hasFeed) {
+                                    Box(
+                                        modifier = Modifier
+                                            .size(8.dp)
+                                            .background(Color.Gray, CircleShape)
+                                            .align(Alignment.CenterHorizontally)
+                                    )
+                                }
+                            }
+
+                        } else {
+                            Box(
+                                modifier = Modifier
+                                    .size(35.dp)
+                                    .background(Color.Transparent, CircleShape)
+                            )
+                        }
+                    }
+                }
+                Spacer(modifier = Modifier.height(16.dp))
+            }
+        }
     }
 }
